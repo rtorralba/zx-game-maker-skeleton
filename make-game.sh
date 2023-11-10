e36fc9477decc4d1708fae96393262dd1f2782d8
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2023-11-10 20:34:53 +0100

    execute as a root

--- make-game.sh
+++ make-game.sh
@@ -1,2 +1,2 @@
 tiled --export-map json assets/maps.tmx output/maps.json
-docker run -u $(id -u):$(id -g) -v ${PWD}/assets:/app/assets -v ${PWD}/output:/app/output -it --rm rtorralba/zx-game-maker:0.0.1
+docker run -v ${PWD}/assets:/app/assets -v ${PWD}/output:/app/output -it --rm rtorralba/zx-game-maker:0.0.1
