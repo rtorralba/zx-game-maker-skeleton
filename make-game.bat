859b5b6fcb81fdbd873eee6a4c89663add61faaa
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2023-11-11 14:13:14 +0100

    Added make-game.bat

---
+++ make-game.bat
@@ -0,0 +1,2 @@
+"c:\Program Files\Tiled\tiled.exe" --export-map json assets/maps.tmx output/maps.json
+docker run -v %cd%/assets:/app/assets -v %cd%/output:/app/output -it --rm rtorralba/zx-game-maker:0.0.1
