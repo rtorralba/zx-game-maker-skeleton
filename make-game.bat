45a63fab3c16dbc886657d2a1214cafe63dd88a0
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2023-11-12 11:54:30 +0100

    use always latest game maker version

--- make-game.bat
+++ make-game.bat
@@ -1,3 +1,4 @@
 "c:\Program Files\Tiled\tiled.exe" --export-map json assets/maps.tmx output/maps.json
-docker run -v %cd%/assets:/app/assets -v %cd%/output:/app/output -it --rm rtorralba/zx-game-maker:0.0.1
+docker pull rtorralba/zx-game-maker
+docker run -v %cd%/assets:/app/assets -v %cd%/output:/app/output -it --rm rtorralba/zx-game-maker
 pause
