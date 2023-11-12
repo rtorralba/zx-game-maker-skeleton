bc40ddc7028cca90f14c96fc85c3f05c017b5398
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2023-11-12 11:53:52 +0100

    use always latest game maker version

--- make-game.sh
+++ make-game.sh
@@ -1,2 +1,3 @@
 tiled --export-map json assets/maps.tmx output/maps.json
-docker run -v ${PWD}/assets:/app/assets -v ${PWD}/output:/app/output -it --rm rtorralba/zx-game-maker:0.0.1
+docker pull rtorralba/zx-game-maker
+docker run -v ${PWD}/assets:/app/assets -v ${PWD}/output:/app/output -it --rm rtorralba/zx-game-maker
