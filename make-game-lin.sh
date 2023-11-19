1e8a42a29c9d82e579d7b4b1c1b9ab25378ca9c0
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2023-11-19 03:25:32 +0100

    improve scripts

--- make-game-lin.sh
+++ make-game-lin.sh
@@ -1,3 +1,3 @@
 tiled --export-map json assets/maps.tmx output/maps.json
 docker pull rtorralba/zx-game-maker
-docker run -v "${PWD}"/assets:/app/assets -v "${PWD}"/output:/app/output -it --rm rtorralba/zx-game-maker
+docker run -v "${PWD}"/assets:/app/assets -v "${PWD}"/output:/app/output -u $(id -u):$(id -g) -it --rmrtorralba/zx-game-maker
