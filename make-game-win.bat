9621083655f53d899bfc83a2705e1714b73a64e5
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2024-01-08 19:45:30 +0100

    generate game into dist

--- make-game-win.bat
+++ make-game-win.bat
@@ -1,4 +1,4 @@
 "c:\Program Files\Tiled\tiled.exe" --export-map json assets/maps.tmx output/maps.json
 docker pull rtorralba/zx-game-maker
-docker run -v %cd%/assets:/app/assets -v %cd%/output:/app/output -it --rm rtorralba/zx-game-maker
+docker run -v %cd%/assets:/app/assets -v %cd%/output:/app/output -v %cd%/dist:/app/dist -it --rm rtorralba/zx-game-maker
 pause
